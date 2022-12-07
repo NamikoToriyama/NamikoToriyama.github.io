3d749a323dd1e71f320170766c75684a84930be9
NamikoToriyama <namiko.trym@gmail.com>
2022-12-07 10:53:40 +0000

    Deploy 47dc3aa14ff8fa6c6c58f2183315f6c428924308 by GitHub Actions

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.39f5295e3196730ceb1f1825a45859b4.js"
+  "./precache-manifest.36fca8ee1f166038a490a0df32ff3979.js"
 );
 
 self.addEventListener('message', (event) => {
